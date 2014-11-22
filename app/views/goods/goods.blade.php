575fc5c172079b517dbe384142de237ed21049b9
nsgbljh <irinalekhova@gmail.com>
2014-11-22 20:21:07 +0300

    Доделала отображение товаров на главной

--- app/views/goods/goods.blade.php
+++ app/views/goods/goods.blade.php
@@ -1,15 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title></title>
-	<meta charset="utf-8">
+	<meta charset="UTF-8">
+	<title>Irina Project</title>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<ul>
-<?php foreach($item as $good):?>
-	<li>{{ $good->price }}</li>
-	<li>{{ $good->name }}</li>
-<?php endforeach;?>
-</ul>
+
+<div class="navbar navbar-mad">
+	<div class="container">
+
+		<div class="navbar-header">
+			<a href="#" class="navbar-brand">Brand</a>
+		</div>
+
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="#">
+				<i class="fa fa-shopping-cart"></i>
+				<span class="price">300$</span>
+			</a></li>
+		</ul>
+
+	</div>
+</div>
+
+<div class="container">
+	
+	<div class="row">
+		<?php foreach($item as $good):?>
+		<div class="col-sm-4">
+			<img src="img/items/{{ $good->filename }}" class="img-responsive text-center" alt="" width="200" >
+			<h4>{{ $good->name }}</h4>
+			<div class="row">
+				<div class="col-sm-6">
+					<h5>{{ $good->price , ' руб.' }}</h5>
+				</div>
+				<div class="col-sm-6">
+					<button class="btn btn-primary">Buy Now</button>
+				</div>
+			</div>
+		</div>
+		<?php endforeach;?>
+	</div>
+</div>
 </body>
 </html>
